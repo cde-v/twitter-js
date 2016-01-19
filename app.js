c73440b72c1cef4ba9df04a9ba4197be881fab0f
Chris de Villiers <cpdevill@gmail.com>
2016-01-19 15:04:29 -0500

    Swig functionality working

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
+var chalk = require("chalk");
+var swig = require("swig");
 var express = require("express");
 var app = express();
-var chalk = require("chalk");
 
-app.use("*/special/", function(req, res, next) {
-	res.send(chalk.red(req.method + req.originalUrl));
-		next();
-})
+var locals = {
+	title: "An Example",
+	people: [{
+		name: "Gandalf"}, {
+		name: "Frodo"}, {
+	 	name: "Hermione"}]
+};
 
-app.use("/", function(req, res, next) {
-	res.send(chalk.blue(req.method + req.originalUrl));
-})
+app.engine("html", swig.renderFile);
 
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
 
-
-app.get("/", function(req, res) {
-	res.send("we got the get");
+app.get("/views", function (req, res) {
+	res.render("index", locals)
 });
 
-app.get("/news", function(req, res) {
-	res.send("we got the news get");
-});
+
+
+
+
+// app.use("*/special/", function(req, res, next) {
+// 	res.send(chalk.red(req.method + req.originalUrl));
+// 		next();
+// })
+
+// app.use("/", function(req, res, next) {
+// 	res.send(chalk.blue(req.method + req.originalUrl));
+// })
+
+// app.get("/", function(req, res) {
+// 	res.send("we got the get");
+// });
+
+// app.get("/news", function(req, res) {
+// 	res.send("we got the news get");
+// });
 
 
 
@@ -29,5 +49,3 @@ app.get("/news", function(req, res) {
 app.listen(3000, function() {
 	console.log("server listening")
 });
-
-WHATS UP CHRIS?!
