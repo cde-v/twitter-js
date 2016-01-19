d9de7f300fb63ef9e1970e38950534742b7da8a8
Chris de Villiers <cpdevill@gmail.com>
2016-01-19 14:33:15 -0500

    added package.json script

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var chalk = require("chalk");
+
+app.use("*/special/", function(req, res, next) {
+	res.send(chalk.red(req.method + req.originalUrl));
+		next();
+})
+
+app.use("/", function(req, res, next) {
+	res.send(chalk.blue(req.method + req.originalUrl));
+})
+
+
+
+app.get("/", function(req, res) {
+	res.send("we got the get");
+});
+
+app.get("/news", function(req, res) {
+	res.send("we got the news get");
+});
+
+
+
+
+
+
+app.listen(3000, function() {
+	console.log("server listening")
+});
